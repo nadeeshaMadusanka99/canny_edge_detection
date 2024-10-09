26769e7e9912919f83c1dee2630afeb799991a0b
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-09 11:13:21 +0530

    Add gray scale conversion

--- 200614N_canny_edge_detector_script.py
+++ 200614N_canny_edge_detector_script.py
@@ -3,6 +3,12 @@ import numpy as np
 from PIL import Image
 
 
+def rgb_to_grayscale(rgb_image):
+    r, g, b = rgb_image[:, :, 0], rgb_image[:, :, 1], rgb_image[:, :, 2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray.astype(np.uint8)
+
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python <script_name>.py <input_image>")
@@ -13,6 +19,12 @@ def main():
 
     # Load image
     image_array = np.array(Image.open(input_name))
+
+    # Convert image to grayscale
+    if len(image_array.shape) == 3:
+        gray_image = rgb_to_grayscale(image_array)
+    else:
+        gray_image = image_array
 
 
 if __name__ == "__main__":
